75431643230f4c41b479e4f1a83678d92dc19e0f
volkova-lisa <liza.volk999@gmail.com>
2021-10-06 16:31:21 +0300

    ui template done

--- app/src/main/java/quizapp/volkova/notesapp/MainActivity.kt
+++ app/src/main/java/quizapp/volkova/notesapp/MainActivity.kt
@@ -2,10 +2,26 @@ package quizapp.volkova.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import quizapp.volkova.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //это нужно чтобы избежать утечки памяти!! запомнить*
+        _binding = null
     }
 }
