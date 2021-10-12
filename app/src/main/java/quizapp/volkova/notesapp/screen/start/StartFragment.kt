6e3b483d023e3f65017ff94a0b8533a2ec4b4951
volkova-lisa <liza.volk999@gmail.com>
2021-10-12 18:20:58 +0300

    raw version of timer

---
+++ app/src/main/java/quizapp/volkova/notesapp/screen/start/StartFragment.kt
@@ -0,0 +1,37 @@
+package quizapp.volkova.notesapp.screen.start
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import quizapp.volkova.notesapp.R
+import quizapp.volkova.notesapp.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mViewModel: StartFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialisation()
+    }
+
+    private fun initialisation() {
+        mViewModel = ViewModelProvider(this).get(StartFragmentViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/quizapp/volkova/notesapp/screen/start/StartFragmentViewModel.kt
@@ -0,0 +1,12 @@
+package quizapp.volkova.notesapp.screen.start
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class StartFragmentViewModel(app: Application) : AndroidViewModel(app) {
+    private val mContext = app
+
+    fun initDataBase() {
+
+    }
+}
