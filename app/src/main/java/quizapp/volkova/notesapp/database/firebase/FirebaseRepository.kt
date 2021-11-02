fa9c04953cae0199ab49ac0f0dd8e7d9fe6759f5
volkova-lisa <liza.volk999@gmail.com>
2021-11-02 17:26:03 +0200

    notes are added to firebase

--- app/src/main/java/quizapp/volkova/notesapp/database/firebase/FirebaseRepository.kt
+++ app/src/main/java/quizapp/volkova/notesapp/database/firebase/FirebaseRepository.kt
@@ -2,19 +2,30 @@ package quizapp.volkova.notesapp.database.firebase
 
 import androidx.lifecycle.LiveData
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import quizapp.volkova.notesapp.database.DataBaseRepository
 import quizapp.volkova.notesapp.models.NoteBody
-import quizapp.volkova.notesapp.utils.EMAIL
-import quizapp.volkova.notesapp.utils.PASS
+import quizapp.volkova.notesapp.utils.*
 
 class FirebaseRepository: DataBaseRepository {
 
     private val mAuth = FirebaseAuth.getInstance()
+    private val mDatabaseReference = FirebaseDatabase.getInstance().reference
+        .child(mAuth.currentUser?.uid.toString())
 
     override val allNotes: LiveData<List<NoteBody>> = NotesLiveData()
 
     override suspend fun insert(noteBody: NoteBody, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
+        val idNote = mDatabaseReference.push().key.toString()
+        val noteMap = hashMapOf<String, Any>()
+        noteMap[ID_FIREBASE] = idNote
+        noteMap[NAME] = noteBody.name
+        noteMap[TEXT] = noteBody.text
+
+        mDatabaseReference.child(idNote)
+            .updateChildren(noteMap)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { showToast("Error happened") }
     }
 
     override suspend fun delete(noteBody: NoteBody, onSuccess: () -> Unit) {
--- app/src/main/java/quizapp/volkova/notesapp/utils/consts.kt
+++ app/src/main/java/quizapp/volkova/notesapp/utils/consts.kt
@@ -8,5 +8,8 @@ lateinit var REPOSITORY:DataBaseRepository
 const val TYPE_DATABASE = "type database"
 const val TYPE_ROOM = "type_room"
 const val TYPE_FIREBASE = "type_firebase"
+const val ID_FIREBASE = "id_firebase"
+const val NAME = "name"
+const val TEXT = "text"
 lateinit var EMAIL: String
 lateinit var PASS: String
